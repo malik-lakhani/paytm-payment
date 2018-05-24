7e34f3307eccd0b823f5df866d9a3cab01d51608
bhumika <bhumika@improwised.com>
2018-05-24 12:49:46 +0530

    remove unnecessory content

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,23 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="css/util.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
@@ -104,7 +90,7 @@
                 </div>
 
                 <div class="checkbox">
-                    <label>
+                    <label class="agreed-label">
                       <input
                         type="checkbox"
                         id="agreed"
@@ -120,7 +106,7 @@
                 </div>
             </form>
 
-            <div class="contact100-more flex-col-c-m" style="background-image: url('images/bg-01.jpg');">
+            <div class="contact100-more flex-col-c-m">
                 <div class="flex-w size1 p-b-47">
                     <div class="flex-col size2">
                         <span class="txt1 p-b-20">
@@ -128,7 +114,7 @@
                         </span>
 
                         <span class="txt3">
-                            You can generate ticket of price {{ config('app.ticket_price') }} /-
+                            You can generate ticket of price <br><span class="price-symbol">₹ {{ config('app.ticket_price') }}</span>
                         </span>
                     </div>
                 </div>
@@ -143,33 +129,9 @@
 <!--===============================================================================================-->
     <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-    <script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
     <script src="vendor/bootstrap/js/popper.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-    <script src="vendor/select2/select2.min.js"></script>
-    <script>
-        $(".selection-2").select2({
-            minimumResultsForSearch: 20,
-            dropdownParent: $('#dropDownSelect1')
-        });
-    </script>
-<!--===============================================================================================-->
-    <script src="vendor/daterangepicker/moment.min.js"></script>
-    <script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-    <script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
     <script src="js/main.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-23581568-13');
-    </script>
 </body>
 </html>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -5,13 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.ico') }}"/>
-<!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
 <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/admin.css') }}">
 </head>
